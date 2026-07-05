2df02b8dc19f46e654d048ef76c36b873459aace
Alaestor Weissman <alaestor@0x04.cc>
2026-07-05 09:29:17 -0400

    doc(alce): missed some comments

--- alce/src/globals.lua
+++ alce/src/globals.lua
@@ -18,10 +18,10 @@ These global config options can be set from anywhere at any time. Some functions
 About magic numbers:
 - `isAddress_userspaceBoundary32` defaults to a "3g split"; set `0x7FFFFFFF` for 2GB
 - `isOffset_tooFarBoundary` expects offsets to be < 4096 bytes
-- `isAddress_userspaceBoundary64`
+- `isAddress_userspaceBoundary64` is a number that came to me in a dream
 
 ]],
-        __doc_verbatim = true,
+        __doc_verbatim = true, -- print KV pairs verbatim in documentation
 
         debug_print = false,
         warn_print = true,
