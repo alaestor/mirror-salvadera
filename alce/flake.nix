49dfe6394f86c6b620be5b13cf912418e5e2913e
Alaestor Weissman <alaestor@0x04.cc>
2026-07-06 10:42:05 -0400

    feat(alce): package luapack

--- alce/flake.nix
+++ alce/flake.nix
@@ -17,21 +17,18 @@
         "aarch64-darwin"
       ];
 
-      perSystem =
-        { pkgs, ... }:
-        let
-          commonDeps = with pkgs; [
-            lua5_3
-          ];
-        in
-        {
+      perSystem = { pkgs, self', ... }: {
+
           devShells.default = pkgs.mkShell {
-            packages = commonDeps;
+            packages = [
+              pkgs.lua5_3
+              self'.packages.luapack
+            ];
 
             shellHook = ''
               echo "Development environment activated"
               export LUA_PATH="../?.lua;;"
-              python --version
+              luapack --version
               lua -v
             '';
           };
@@ -71,6 +68,35 @@
                 '';
               }
             );
+          };
+
+          packages.default = self'.packages.alcelib;
+
+          #TODO packages.alcelib = ...
+
+          packages.luapack = let
+            pname = "luapack";
+            version = "0.1.1";
+            src = pkgs.fetchFromGitHub {
+              owner = "the-unnamed-goose";
+              repo = pname;
+              rev = "v${version}";
+              hash = "sha256-WDBIF7eiRUdmhWYZG4Gbi1p0p6d5CYHVbXTFRbofpWM=";
+            };
+          in pkgs.rustPlatform.buildRustPackage {
+            inherit pname version src;
+
+            cargoLock.lockFile = "${src}/Cargo.lock";
+
+            doCheck = false;
+
+            meta = with pkgs.lib; {
+              description = "A basic rust application for efficiently bundling Lua scripts into monolithic releases.";
+              homepage = "https://github.com/the-unnamed-goose/luapack";
+              license = licenses.mpl20;
+              mainProgram = "luapack";
+              platforms = platforms.unix;
+            };
           };
         };
     };
